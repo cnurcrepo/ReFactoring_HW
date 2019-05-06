a8abdd9ba53826c72ba64623f32b75561f33e28b
jopemachine <jopemachine@naver.com>
2019-05-06 16:29:02 +0900

    Add 3 Test Class Files

---
+++ Refactoring_GyuBong/test_DropItem/Monster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 주의 : 이 클래스는 Monobehavior를 상속받지 않아야 함
+/// 그래서 Adapter 클래스를 따로 만들었다.
+/// </summary>
+
+namespace UnityRPG_UnitTest
+{
+    [Serializable]
+    public class Monster
+    {
+        public int ID;
+        public string Name;
+        public string Description;
+        public int MaxHP;
+        public int MaxMP;
+        public int ExperienceValue;
+        public int Speed;
+        public GameObject MonsterModel;
+
+        // monsterDropItems는 몬스터가 드롭할 수 있는 아이템들과 확률. dropItemProbAccum는 그 확률들을 누적해 더해 놓은 것으로, MonsterParser에서 계산.
+        public List<MonsterDropItem> monsterDropItems;
+        public float[] dropItemProbAccum;
+
+        public Monster getCopy()
+        {
+            return (Monster)this.MemberwiseClone();
+        }
+
+        public Monster() {
+            monsterDropItems = new List<MonsterDropItem>();
+        }
+        
+
+    }
+}
---
+++ Refactoring_GyuBong/test_DropItem/MonsterDropItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 주의 : 이 클래스는 Monobehavior를 상속받지 않아야 함
+/// 그래서 Adapter 클래스를 따로 만들었다.
+/// </summary>
+
+namespace UnityRPG_UnitTest
+{
+    [Serializable]
+    public class MonsterDropItem
+    {
+        public int ItemID;
+        public float DropProb;
+        public int DropMinNumber;
+        public int DropMaxNumber;
+        public MonsterDropItem(int _ItemID, float _DropProb, int _DropMinNumber, int _DropMaxNumber)
+        {
+            ItemID = _ItemID;
+            DropProb = _DropProb;
+            DropMinNumber = _DropMinNumber;
+            DropMaxNumber = _DropMaxNumber;
+        }
+
+    }
+}
