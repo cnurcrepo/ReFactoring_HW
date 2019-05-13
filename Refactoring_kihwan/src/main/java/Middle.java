3f64274e3b5c682b04532650d1d34272c5ef6193
kihwankim <clack2933@gmail.com>
2019-05-13 18:14:48 +0900

    make receiveData and send Ack method

--- Refactoring_kihwan/src/main/java/Middle.java
+++ Refactoring_kihwan/src/main/java/Middle.java
@@ -22,7 +22,7 @@ public class Middle {
         private byte[] srcAddr = new byte[6];
         private byte[] dstAddr = new byte[6];
 
-        public EtherNetAddr() {
+        private EtherNetAddr() {
             for (int indexOfAddr = 0; indexOfAddr < srcAddr.length; ++indexOfAddr) {
                 this.srcAddr[indexOfAddr] = (byte) 0x00;
                 this.dstAddr[indexOfAddr] = (byte) 0x00;
@@ -30,9 +30,31 @@ public class Middle {
         }
     }
 
-    public boolean receiveFrameData(byte[] inputData) {
+    public boolean receiveFrameData(byte[] inputData) {//다름 class에서 호출해야함
+        if (this.isBoardData(inputData) || this.isMyConnectionData(inputData)) {
+            if (inputData[13] == (byte) 0x01) {
+                this.sendEthernetAck(inputData);
+                byte[] removeHeaderData = removeCappHeaderData(inputData);
+                return true;// 원래 코드에서는 상위 layer에 data보내야 하는데 일부분이라서 생략헀습니다.
+            } else if (inputData[13] == (byte) 0x02) {//ack이므로 상위 계층에 알리고 false return
+                return false;
+            }
+        }
+        return false;//내가 원한 frame이 아니다
+    }
 
-        return false;
+    private boolean sendEthernetAck(byte[] inputData) {
+        byte[] ackFrame = new byte[14];
+        int index = 0;
+        while (index < 6) {
+            ackFrame[index] = inputData[index + 6];
+            ackFrame[index + 6] = inputData[index];
+            index += 1;
+        }
+        ackFrame[12] = 0x00;
+        ackFrame[13] = 0x02;
+        //원래 코드에서는 하위 계층으로 호출해서 보내야함
+        return true;
     }
 
     private boolean checkTheFrameData(byte[] myAddressData, byte[] inputFrameData, int inputDataStartIndex) {
@@ -59,5 +81,11 @@ public class Middle {
         byte[] dstAddr = this._ethernetHeader.getEnetAddr().getDstAddr();
         return this.checkTheFrameData(srcAddr, inputFrameData, 0)
                 && this.checkTheFrameData(dstAddr, inputFrameData, 6);
+    }
+
+    private byte[] removeCappHeaderData(byte[] input) {//header 제거 코드를 따로 빼서작성 receive에 너무 많은기능 구현을 피하기 위한 것
+        byte[] removeCappHeader = new byte[14];
+        System.arraycopy(input, 14, removeCappHeader, 0, 14);
+        return removeCappHeader;
     }
 }
