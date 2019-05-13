f658ee0ac85154ddd99071da812dac4b5f7c6129
kihwankim <clack2933@gmail.com>
2019-05-14 00:20:51 +0900

    Cam file kihwan (#19)

--- Refactoring_kihwan/src/test/java/MiddleCamTest.java
+++ Refactoring_kihwan/src/test/java/MiddleCamTest.java
@@ -1,5 +1,9 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -7,11 +11,47 @@ import static org.junit.Assert.*;
 public class MiddleCamTest {
 
     MiddleCam middleCam;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
     @Before
     public void createMiddleCamTestObj() {
         middleCam = new MiddleCam();
+        System.setOut(new PrintStream(outContent));
+        //upStream
     }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testReceiveFrameDataInBoard() {
+        byte[] boardData = new byte[28];
+        boardData[12] = (byte) 0x00;
+        boardData[13] = (byte) 0x01;
+
+        for (int index = 0; index < 6; index++) {
+            boardData[index] = (byte) 0xFF;
+        }
+        byte[] boardDataNext = boardData.clone();
+        assertThat(this.middleCam.receiveFrameData(boardData), is(false));
+
+
+        byte[] boardSrc = new byte[6];
+        for (int index = 0; index < 6; index++) {
+            boardSrc[index] = (byte) index;
+        }
+        this.middleCam.setSrcAddr(boardSrc);
+        assertThat(this.middleCam.receiveFrameData(boardDataNext), is(true));
+    }
+
+    @Test
+    public void out() {
+        System.out.print("hello");
+    }
+
 
     @Test
     public void testReceiveFrameData() {
@@ -23,6 +63,7 @@ public class MiddleCamTest {
 
         //then
         assertThat(this.middleCam.receiveFrameData(frameData), is(true));
+        assertEquals("send Ack", this.outContent.toString());
     }
 
     @Test
@@ -35,6 +76,7 @@ public class MiddleCamTest {
 
         //then
         assertThat(this.middleCam.receiveFrameData(frameData), is(false));
+        assertEquals("receive ack", this.outContent.toString());
     }
 
 }
